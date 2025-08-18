753863978454396c7818962f5a3852f767afc878
Touhid Tamim <touhidtamim1@gmail.com>
2025-08-18 18:49:43 +0600

    10th problem solved

--- scic-js-problem-solving.js
+++ scic-js-problem-solving.js
@@ -115,3 +115,20 @@ function factorial(n) {
 }
 
 console.log(factorial(5));
+
+// 10th problem
+function pingPong() {
+  for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("PingPong");
+    } else if (i % 3 === 0) {
+      console.log("Ping");
+    } else if (i % 5 === 0) {
+      console.log("Pong");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+pingPong();
