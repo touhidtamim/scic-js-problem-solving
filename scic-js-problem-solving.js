e6e63b79afd84d14fdc44118279093d27adf26c0
Touhid Tamim <touhidtamim1@gmail.com>
2025-08-18 18:40:12 +0600

    problem 3 solved

--- scic-js-problem-solving.js
+++ scic-js-problem-solving.js
@@ -28,3 +28,17 @@ function countVowels(str) {
 console.log(reverseString("hello"));
 console.log(reverseString("Touhid"));
 console.log(reverseString("Bangladesh"));
+
+// 3rd Problem
+
+function isPalindrome(str) {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return str === reversed;
+}
+
+console.log(isPalindrome("level"));
+console.log(isPalindrome("madam"));
+console.log(isPalindrome("tamim"));
