f58d21539cd37b24d07985f6e0dd82a3c80f57de
Touhid Tamim <touhidtamim1@gmail.com>
2025-08-18 18:48:30 +0600

    9th problem solved

--- scic-js-problem-solving.js
+++ scic-js-problem-solving.js
@@ -104,3 +104,14 @@ function capitalizeWords(str) {
 }
 
 console.log(capitalizeWords("hello tamim"));
+
+// 9th problem
+function factorial(n) {
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+console.log(factorial(5));
